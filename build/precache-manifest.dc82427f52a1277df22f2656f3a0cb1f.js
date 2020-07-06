5014d7b36afc13adbf6088ce5cef7853f0cd1a19
papyrinIS <papyrin@sumirea.ru>
2020-07-06 10:20:25 +0300

    v1

---
+++ build/precache-manifest.dc82427f52a1277df22f2656f3a0cb1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69786427716e0bf83a2ac0781624366",
+    "url": "/first/index.html"
+  },
+  {
+    "revision": "f295d16a0d7f424d475e",
+    "url": "/first/static/css/main.9938d1bb.chunk.css"
+  },
+  {
+    "revision": "6a4a37ee284022508515",
+    "url": "/first/static/js/2.45bbed4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first/static/js/2.45bbed4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f295d16a0d7f424d475e",
+    "url": "/first/static/js/main.ff287ddf.chunk.js"
+  },
+  {
+    "revision": "289746636d8b720638b5",
+    "url": "/first/static/js/runtime-main.668074be.js"
+  },
+  {
+    "revision": "b0081b7a93fc367ad4b9edbbc39b2883",
+    "url": "/first/static/media/Preloader.b0081b7a.gif"
+  },
+  {
+    "revision": "d35256266f169115dfe17ff5a59219b0",
+    "url": "/first/static/media/basket.d3525626.svg"
+  },
+  {
+    "revision": "5ee48e6525366e3669eb62d0444c0513",
+    "url": "/first/static/media/inst.5ee48e65.png"
+  },
+  {
+    "revision": "7dc6e5d53d4ab8beede455bf624080c5",
+    "url": "/first/static/media/star.7dc6e5d5.svg"
+  },
+  {
+    "revision": "d1d08460bfd28080eb4d570f6baeaa60",
+    "url": "/first/static/media/starWhite.d1d08460.svg"
+  },
+  {
+    "revision": "9a50426b2123777f2bd9490fa67d0d54",
+    "url": "/first/static/media/vk.9a50426b.png"
+  }
+]);
